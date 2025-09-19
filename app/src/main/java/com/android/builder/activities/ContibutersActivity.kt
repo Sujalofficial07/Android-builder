ec4a6b299c4e2afd475de6599beadfd135da77c8
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-09-19 12:14:30 +0530

    Update ContibutersActivity.kt

--- app/src/main/java/com/android/builder/activities/ContibutersActivity.kt
+++ app/src/main/java/com/android/builder/activities/ContibutersActivity.kt
@@ -1,20 +1,3 @@
-/*
- *  This file is part of Android-builder.
- *
- *  Android-builder is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Android-builder is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *   along with Android-builder.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package com.android.builder.activities
 
 import android.os.Bundle
@@ -24,21 +7,19 @@ import androidx.core.graphics.Insets
 import androidx.core.view.isVisible
 import com.android.builder.R
 import com.android.builder.adapters.ContributorsGridAdapter
-import com.android.builder.app.EdgeToEdgeIDEActivity
+import com.android.builder.app.EdgeToEdgeActivity
 import com.android.builder.databinding.ActivityContributorsBinding
 import com.android.builder.utils.getConnectionInfo
 import com.android.builder.viewmodel.ContributorsViewModel
 
 /**
- * Contributors screen for Android-builder.
+ * Displays project contributors and translators.
  */
-class ContributorsActivity : EdgeToEdgeIDEActivity() {
+class ContributorsActivity : EdgeToEdgeActivity() {
 
     private var _binding: ActivityContributorsBinding? = null
     private val binding: ActivityContributorsBinding
-        get() = checkNotNull(_binding) {
-            "Activity has been destroyed"
-        }
+        get() = checkNotNull(_binding) { "Activity has been destroyed" }
 
     private val viewModel by viewModels<ContributorsViewModel>()
 
@@ -56,25 +37,21 @@ class ContributorsActivity : EdgeToEdgeIDEActivity() {
             supportActionBar!!.setDisplayHomeAsUpEnabled(true)
             toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
 
-            githubContributors.apply {
-                sectionTitle.setText(R.string.title_github_contributors)
-            }
-
-            // Rebranded: no Crowdin section, keep it general
-            translationContributors.apply {
-                sectionTitle.setText(R.string.title_translators)
-            }
+            githubContributors.sectionTitle.setText(R.string.title_github_contributors)
+            translationContributors.sectionTitle.setText(R.string.title_crowdin_translators)
 
             noConnection.root.setText(R.string.msg_no_internet)
             loadingProgress.isVisible = false
         }
 
         viewModel._crowdinTranslators.observe(this) { translators ->
-            binding.translationContributors.sectionItems.adapter = ContributorsGridAdapter(translators)
+            binding.translationContributors.sectionItems.adapter =
+                ContributorsGridAdapter(translators)
         }
 
         viewModel._githubContributors.observe(this) { githubContributors ->
-            binding.githubContributors.sectionItems.adapter = ContributorsGridAdapter(githubContributors)
+            binding.githubContributors.sectionItems.adapter =
+                ContributorsGridAdapter(githubContributors)
         }
 
         val connectionInfo = getConnectionInfo(this)
@@ -87,7 +64,6 @@ class ContributorsActivity : EdgeToEdgeIDEActivity() {
                 viewModel.observeLoadingState(this@ContributorsActivity) { isLoading ->
                     binding.loadingProgress.isVisible = isLoading
                 }
-
                 viewModel.fetchAll()
             }
         }
@@ -95,14 +71,12 @@ class ContributorsActivity : EdgeToEdgeIDEActivity() {
 
     override fun onApplySystemBarInsets(insets: Insets) {
         super.onApplySystemBarInsets(insets)
-        binding.toolbar.apply {
-            setPaddingRelative(
-                paddingStart + insets.left,
-                paddingTop,
-                paddingEnd + insets.right,
-                paddingBottom
-            )
-        }
+        binding.toolbar.setPaddingRelative(
+            binding.toolbar.paddingStart + insets.left,
+            binding.toolbar.paddingTop,
+            binding.toolbar.paddingEnd + insets.right,
+            binding.toolbar.paddingBottom
+        )
     }
 
     override fun onDestroy() {
